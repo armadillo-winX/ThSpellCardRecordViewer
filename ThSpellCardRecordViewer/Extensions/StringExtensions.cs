bbc7c1b120c80dc1c538a2beeb6bf945fc96421a
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-08-30 23:33:21 +0900

    StringExtensions実装

---
+++ ThSpellCardRecordViewer/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ThSpellCardRecordViewer.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static string TranslateUnixTime(this int unixTime)
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(unixTime).LocalDateTime.ToString();
+        }
+
+        public static int TranslateHexadecimal(this string hexadecimal)
+        {
+            return Convert.ToInt32(hexadecimal, 16);
+        }
+    }
+}
