288bb13d82b009132a8ba2d007363defe1ebd380
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-12-20 00:48:51 +0900

    ApplicationThemeクラス実装

---
+++ ThSpellCardRecordViewer/ApplicationTheme.cs
@@ -0,0 +1,47 @@
+﻿using DynamicAero2;
+
+namespace ThSpellCardRecordViewer
+{
+    internal class ApplicationTheme
+    {
+        private static string? _themeName;
+
+        public static string? ThemeName
+        {
+            get
+            {
+                return _themeName;
+            }
+
+            set
+            {
+                _themeName = value;
+                SetApplicationTheme(_themeName);
+            }
+        }
+
+        private static void SetApplicationTheme(string? themeName)
+        {
+            Theme theme = Application.Current.Resources.MergedDictionaries[0] as Theme;
+            if (theme != null)
+            {
+                if (themeName == "Light")
+                {
+                    theme.Color = ThemeColor.Light;
+                }
+                else if (themeName == "Dark")
+                {
+                    theme.Color = ThemeColor.Dark;
+                }
+                else if (themeName == "Black")
+                {
+                    theme.Color = ThemeColor.Black;
+                }
+                else
+                {
+                    theme.Color = ThemeColor.NormalColor;
+                }
+            }
+        }
+    }
+}
