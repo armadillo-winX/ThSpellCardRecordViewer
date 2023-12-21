0d1c039bedc11a80207f4112a199d4311d730fc6
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-12-21 22:52:54 +0900

    IndividualSpellCardRecordDataクラス実装

---
+++ ThSpellCardRecordViewer/Score/IndividualSpellCardRecordData.cs
@@ -0,0 +1,13 @@
+﻿namespace ThSpellCardRecordViewer.Score
+{
+    internal class IndividualSpellCardRecordData
+    {
+        public string? Player { get; set; }
+
+        public string? Challenge { get; set; }
+
+        public string? Get { get; set; }
+
+        public string? GetRate { get; set; }
+    }
+}
