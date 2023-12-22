606993cdb03e704cd90704786008a5c3f184075a
Mashiro Tamane <80902436+armadillo-winX@users.noreply.github.com>
2023-12-23 01:52:37 +0900

    RemoveRightOfメソッドを削除

--- ThSpellCardRecordViewer/Extensions/StringExtensions.cs
+++ ThSpellCardRecordViewer/Extensions/StringExtensions.cs
@@ -11,16 +11,5 @@
         {
             return Convert.ToInt32(hexadecimal, 16);
         }
-
-        public static string RemoveRightOf(this string text, string removeLetter)
-        {
-            int length = text.IndexOf(removeLetter);
-            if (length < 0)
-            {
-                return text;
-            }
-
-            return text.Substring(0, length);
-        }
     }
 }
